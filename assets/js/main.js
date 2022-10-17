577a8596ee79ea5b298f29446054565db71f20a9
samandarrza <samandarshr@code.edu.az>
2022-10-18 02:36:39 +0400

    add

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function one(){
+    document.getElementById('image_shower').src = './assets/img/1.jpg';
+}
+function two(){
+    document.getElementById('image_shower').src = './assets/img/2.jpg';
+}
+function three(){
+    document.getElementById('image_shower').src = './assets/img/3.jpg';
+}
+function four(){
+    document.getElementById('image_shower').src = './assets/img/4.jpg';
+}
